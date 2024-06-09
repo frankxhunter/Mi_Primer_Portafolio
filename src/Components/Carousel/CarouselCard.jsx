1a1d598e6f867d6f5a3f40f84b9944e5cc20d852
frankxhunter <frankxhunter@gmail.com>
2024-06-10 01:33:09 +0200

    Arreglando bug en el carrusel

--- src/Components/Carousel/CarouselCard.jsx
+++ src/Components/Carousel/CarouselCard.jsx
@@ -1,27 +1,18 @@
 /* eslint-disable react/prop-types */
 import "./CarouselCard.css"
-import { CSSTransition } from "react-transition-group";
-import { useRef, useState } from "react";
 export function CarouselCard({ link, image, name, description }) {
 
-    const [hoverOnCard, setHoverOnCard] = useState(false)
-    const nodeRef = useRef(null)
 
 
-    return <div className="container_card" onMouseOver ={()=>setHoverOnCard(true)} onMouseOut ={()=>setHoverOnCard(false)}>
+    return <div className="container_card" >
         <a href={link} target="_blank" className="linkContainer_card" rel="noreferrer">
-            <img className="img_card" src={image} alt="name" />
-            <CSSTransition 
-              in={hoverOnCard}
-              timeout={2000}
-              classNames="animationHover"
-              unmountOnExit
-              nodeRef={nodeRef}>
-                <div ref={nodeRef} className="info_card">
+            <div>
+                <img className="img_card" src={image} alt="name" />
+            </div>
+            <div className="info_card">
                 <h4 className="title_card" >{name}</h4>
                 <span className="description_card">{description}</span>
-                </div>
-            </CSSTransition>
+            </div>
         </a>
 
     </div>
