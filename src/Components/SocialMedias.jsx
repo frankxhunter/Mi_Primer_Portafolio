1696d026f6cb3c2a7cc25479763a13a8f62af3e7
Frank <frankxhunter@gmail.com>
2025-06-15 00:25:15 +0200

    Eliminación del enlace de Instagram y corrección del enlace de Briefcase en el componente SocialMedias

--- src/Components/SocialMedias.jsx
+++ src/Components/SocialMedias.jsx
@@ -1,6 +1,5 @@
 import { SocialLink } from "./SocialLInk"
 import "./SocialMedias.css"
-import { BsInstagram } from "react-icons/bs";
 import { BsLinkedin } from "react-icons/bs";
 import { BsGithub } from "react-icons/bs";
 import { BsBriefcase } from "react-icons/bs";
@@ -9,10 +8,9 @@ export function SocialMedias() {
 
     return <section className="social_media_section">
         <SocialLink icon={<BsGithub  className="icon_social" />} link={"https://shortfy.link/09E5AUln"}></SocialLink>
-        <SocialLink icon={<BsBriefcase  className="icon_social" />} link={"https://shortfy.link/09E5AUln"}></SocialLink>
+        <SocialLink icon={<BsBriefcase  className="icon_social" />} link={"https://shortfy.link/xrvAU5yX"}></SocialLink>
         <SocialLink icon={<BsLinkedin  className="icon_social" />} link={"https://shortfy.link/UukWeQrD"}></SocialLink>
         <SocialLink icon={<BiLogoGmail  className="icon_social" />} link={"mailto:frankmesad24@gmail.com"}></SocialLink>
-        <SocialLink icon={<BsInstagram  className="icon_social" />} link={"https://shortfy.link/waLX1p62"}></SocialLink>
 
     </section>
 }
