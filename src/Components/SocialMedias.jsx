6c906a566aaa83054671e41090c7884d4078ca58
frankxhunter <frankxhunter@gmail.com>
2025-12-19 19:58:06 +0100

    Change link of linkdln

--- src/Components/SocialMedias.jsx
+++ src/Components/SocialMedias.jsx
@@ -9,7 +9,7 @@ export function SocialMedias() {
     return <section className="social_media_section">
         <SocialLink icon={<BsGithub  className="icon_social" />} link={"https://shortfy.link/09E5AUln"}></SocialLink>
         <SocialLink icon={<BsBriefcase  className="icon_social" />} link={"https://shortfy.link/xrvAU5yX"}></SocialLink>
-        <SocialLink icon={<BsLinkedin  className="icon_social" />} link={"https://shortfy.link/kz450Y9u"}></SocialLink>
+        <SocialLink icon={<BsLinkedin  className="icon_social" />} link={"https://shortfy.link/UukWeQrD"}></SocialLink>
         <SocialLink icon={<BiLogoGmail  className="icon_social" />} link={"mailto:frankmesad24@gmail.com"}></SocialLink>
 
     </section>
